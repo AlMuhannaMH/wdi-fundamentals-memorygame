ea57c769f618febdffafd6831af7af3a3c5db91b
Mohammed AlMuhanna <almuhannamh@outlook.com>
2020-01-20 11:50:13 +0300

    testFoo

---
+++ memory_game/helloworld.js
@@ -0,0 +1,12 @@
+// write your code below
+function makeCone(flavor, numberScoops) {
+    console.log("Coming right up! One " + flavor + " ice cream cone with " + numberScoops + " scoops.");
+};
+
+makeCone("chocolate", 3);
+
+function makeCone(flavor, numberScoops) {
+    console.log("Coming right up! One " + flavor + " ice cream cone with " + numberScoops + " scoops.");
+};
+
+makeCone("chocolate", 3);
